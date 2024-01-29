6afc0df402f19cc43794fa21af95ce12f8897d95
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-01-29 17:43:28 +0200

    made functionality

--- script.js
+++ script.js
@@ -89,3 +89,40 @@ const quizData = [
     correct: "d",
   },
 ];
+
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let answer = undefined
+
+loadQuiz();
+function loadQuiz() {
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+  currentQuiz++;
+}
+
+function getSelected() {
+  const answers = document.querySelectorAll("answer");
+  answers.forEach((answer) => {
+    if(answer.checked)
+  });
+}
+submitBtn.addEventListener("click", () => {
+  currentQuiz++;
+  getSelected();
+  // if (currentQuiz < quizData.length) {
+  //   loadQuiz();
+  // } else {
+  //   alert("You finished");
+  // }
+});
