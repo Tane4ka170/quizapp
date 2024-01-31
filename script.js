7357ad50aea4a3e5bf45006233a966b7f5290726
Tane4ka170 <141357975+Tane4ka170@users.noreply.github.com>
2024-01-31 16:17:31 +0200

    final

--- script.js
+++ script.js
@@ -89,7 +89,8 @@ const quizData = [
     correct: "d",
   },
 ];
-
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
@@ -98,31 +99,57 @@ const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0;
-let answer = undefined
+let score = 0;
 
 loadQuiz();
+
 function loadQuiz() {
+  deselectAnswers();
+
   const currentQuizData = quizData[currentQuiz];
+
   questionEl.innerText = currentQuizData.question;
   a_text.innerText = currentQuizData.a;
   b_text.innerText = currentQuizData.b;
   c_text.innerText = currentQuizData.c;
   d_text.innerText = currentQuizData.d;
-  currentQuiz++;
 }
 
 function getSelected() {
-  const answers = document.querySelectorAll("answer");
-  answers.forEach((answer) => {
-    if(answer.checked)
+  let answer = undefined;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
   });
 }
+
 submitBtn.addEventListener("click", () => {
-  currentQuiz++;
-  getSelected();
-  // if (currentQuiz < quizData.length) {
-  //   loadQuiz();
-  // } else {
-  //   alert("You finished");
-  // }
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+    }
+  }
 });
